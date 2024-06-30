e3c024ec7c94f3543defc52b4139c1ddae096027
AmilSrinath <amilsrinath5@gmail.com>
2024-06-30 18:27:37 +0530

    first commit

---
+++ user-service/src/main/java/lk/ijse/gdse/userservice/controller/UserController.java
@@ -0,0 +1,17 @@
+package lk.ijse.gdse.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Amil Srinath
+ */
+@RestController
+@RequestMapping("api/v1/user")
+public class UserController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "User service is up and running";
+    }
+}
